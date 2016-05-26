5f518594c05502762503bb1209fc3dcd407b0016
Journo-App <creditcalx@gmail.com>
2016-05-25 19:58:10 -0700

    Update database.py
     SELECT name, year, warm_month, cold_month, average_temp
       ...> FROM weather
       ...> INNER JOIN cities 
       ...> ON name = city;

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
 import sqlite3 as lite
+import pandas as pd
+import sys
+
 
 con = lite.connect('getting_started.db') 
-month = raw_input('Enter a month: ')
+# month = raw_input('Enter a month: ')
 
 with con:
 
     cur = con.cursor()
+    
     cur.execute("DROP TABLE IF EXISTS weather");
     cur.execute("DROP TABLE IF EXISTS cities");
+    
     cur.execute("CREATE TABLE cities(name text, state text)");
     cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_temp integer)");
     cur.execute("INSERT INTO cities VALUES('Washington', 'DC')")
@@ -15,16 +20,20 @@ with con:
     cur.execute("INSERT INTO weather VALUES('Washington', 2013, 'July', 'January', 88)")
     cur.execute("INSERT INTO weather VALUES('Houston', 2013, 'July', 'January', 87)")
 
-# cur.execute("SELECT * FROM cities")
-# rows = cur.fetchall()
-# for row in rows:
-#     print(row)
+    cur.execute("SELECT * FROM cities")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-
-SELECT name, state, city, warm_month
-FROM cities 
-INNER JOIN weather
-ON name = city WHERE warm_month='July';
+# join data in sqlite
+with con:
+    cur = con.cursor()
+    
+    NOT WORKING: 
+    # cur.execute("SELECT weather.name, weather.year, weather.warm_month, weather.cold_month, weather.average_temp
+    # FROM weather
+    # INNER JOIN cities 
+    # ON name = cities.city;")
 
 with con:
 
@@ -36,4 +45,4 @@ with con:
   cols = [desc[0] for desc in cur.description]
   df = pd.DataFrame(rows, columns=cols)
 
-  print("The cities that are warmest in " + month + " are: " + ", ".join(df["name"].values))
+
